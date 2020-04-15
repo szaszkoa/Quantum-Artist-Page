26141f777ef15e943cf63fe7d3e24846f9ff49a8
Szaszko <szaszkoa@gmail.com>
2020-04-15 23:03:51 +0200

    QAP-006
    Configure React after ejection to have build path pointed to backend
    templates. Add package.json and script to main folder to easy start of
    application.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 import flask
 
-app = flask.Flask("__main__")
+app = flask.Flask(__name__)
 
 
 @app.route("/")
-def my_index():
+def serve_index():
     return flask.render_template("index.html", token="kiscica")
 
 
-app.run(debug=True)
+# mandatory Hello World until i figure out how to render more pages in the templates folder beside index.html
+@app.route("/home")
+def serve_help():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ backend/static/react/precache-manifest.5bf233df6b927db8a3f04113df8f214c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874c4c035170e271603a84f7bdbea9eb",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "224599f1040eb1770451",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "36e75751e40f69941668",
+    "url": "/static/react/js/2.bf062c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.bf062c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224599f1040eb1770451",
+    "url": "/static/react/js/main.4530cb20.chunk.js"
+  },
+  {
+    "revision": "cfedf8522635b13c864d",
+    "url": "/static/react/js/runtime-main.2d129c54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
